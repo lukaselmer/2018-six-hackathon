dbd34ece089aaadedc38b63f1568abd72157045c
Marion Schleifer <marion.schleifer@simplificator.com>
2018-03-17 23:45:34 +0100

    move comments

--- src/goals/Goal.tsx
+++ src/goals/Goal.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 import * as db from '../interfaces/db';
+import Comments from './Comments';
 
-type P = { id: string, db: db.DBStructure };
+type P = { id: string; db: db.DBStructure };
 
 export default class Goal extends React.Component<P> {
   render() {
-    return <div>{this.goal && this.goal.name}</div>;
+    return (
+      <div>
+        <div>{this.goal && this.goal.name}</div>
+        <div>
+          <Comments
+            comments={[
+              { name: 'Marion', comment: 'I achieved my dream :D I am so happy!' },
+              { name: 'Lukas', comment: 'Absolutely love it!' }
+            ]}
+          />
+        </div>
+      </div>
+    );
   }
 
   private get goal() {
