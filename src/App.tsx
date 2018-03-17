9afce085dd86749272411ef05b601a448f64399c
Lukas Elmer <lukas.elmer@gmail.com>
2018-03-17 22:05:49 +0100

    add material and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import SubscribedGoals from './goals/SubscribedGoals';
 import Goals from './goals/Goals';
 import Goal from './goals/Goal';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { database } from 'firebase';
 import { DBStructure } from './interfaces/db';
+import { Toolbar, AppBar, IconButton, Typography } from 'material-ui';
+import { AccountCircle, Menu as MenuIcon } from 'material-ui-icons';
 
 type S = { db: DBStructure };
 
-class App extends React.Component<{}, S> {
+export default class App extends React.Component<{}, S> {
   firebaseRef: database.Reference;
   firebaseCallback: (a: firebase.database.DataSnapshot | null, b?: string) => string;
 
@@ -30,17 +32,47 @@ class App extends React.Component<{}, S> {
     if (!this.state || !this.state.db) return 'Loading data, please wait...';
 
     return (
-      <div className="App">
-        <header>
-          <ul>
-            <li>
-              <Link to={`/`}>Challenges</Link>
-            </li>
-            <li>
-              <Link to={`/my-goals`}>My Goals</Link>
-            </li>
-          </ul>
-        </header>
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography style={{ flexGrow: 1 }} variant="title" color="inherit">
+              TODO: add logo
+            </Typography>
+            <div>
+              <IconButton color="inherit">
+                <AccountCircle />
+              </IconButton>
+              {/* <IconButton
+              aria-owns={open ? 'menu-appbar' : null}
+              aria-haspopup="true"
+              onClick={() => console.log('this.handleMenu')}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton> */}
+              {/* <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              open={open}
+              onClose={this.handleClose}
+            >
+              <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+              <MenuItem onClick={this.handleClose}>My account</MenuItem>
+            </Menu> */}
+            </div>
+          </Toolbar>
+        </AppBar>
         <Switch>
           <Route exact={true} path="/" render={() => <Goals goals={this.state.db.goals} />} />
           <Route path="/goals/:id" render={props => this.renderGoal(props.match.params.id)} />
@@ -49,6 +81,23 @@ class App extends React.Component<{}, S> {
       </div>
     );
   }
+  //   <div className="App">
+  //   <header>
+  //     <ul>
+  //       <li>
+  //         <Link to={`/`}>Challenges</Link>
+  //       </li>
+  //       <li>
+  //         <Link to={`/my-goals`}>My Goals</Link>
+  //       </li>
+  //     </ul>
+  //   </header>
+  //   <Switch>
+  //     <Route exact={true} path="/" render={() => <Goals goals={this.state.db.goals} />} />
+  //     <Route path="/goals/:id" render={props => this.renderGoal(props.match.params.id)} />
+  //     <Route path="/my-goals" render={() => this.renderSubscribedGoals()} />
+  //   </Switch>
+  // </div>
 
   private renderGoal(goalId: string) {
     return <Goal id={goalId} db={this.state.db as DBStructure} />;
@@ -58,5 +107,3 @@ class App extends React.Component<{}, S> {
     return <SubscribedGoals db={this.state.db as DBStructure} />;
   }
 }
-
-export default App;
