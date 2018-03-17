b2ecd3d2ccbe7639014388c1e77074ec4b57a305
Lukas Elmer <lukas.elmer@gmail.com>
2018-03-17 15:56:35 +0100

    Update db schema

--- src/interfaces/db.ts
+++ src/interfaces/db.ts
@@ -1,10 +1,51 @@
-export type Item = {
-  id: string;
-  label?: string;
-};
+export interface DBStructure {
+  availableGoals: AvailableGoal[];
+  customers: Customer[];
+}
 
-export type DBStructure = {
-  menu: {
-    items: Item[];
-  };
-};
+export interface AvailableGoal {
+  comments: Comment[];
+  goal: Goal;
+}
+
+export interface Customer {
+  badges: string[];
+  id: string;
+  image: string;
+  mail: string;
+  mobile: string;
+  name: string;
+  peers: string;
+  subscribedGoals: SubscribedGoal[];
+  username: string;
+}
+
+export interface SubscribedGoal {
+  actualBalance: number;
+  goal: Goal;
+  progress: number;
+  startingAt: string;
+  startingBalance: number;
+}
+
+export interface Goal {
+  actualTotalSaving: number;
+  benefit: string;
+  deadline: string;
+  description: string;
+  numberOfFollowers: number;
+  image: string;
+  name: string;
+  peers: string;
+  progress: number;
+  rewardImage: string;
+  rewardTextSuccess: string;
+  target: number;
+  targetTotalSaving: number;
+  vendor: string;
+}
+
+export interface Comment {
+  image: string;
+  text: string;
+}
