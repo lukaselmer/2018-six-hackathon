5daa340085d75b5c6dfc510b23ab27ae1a2f9c7e
Lukas Elmer <lukas.elmer@gmail.com>
2018-03-17 16:19:28 +0100

    Add available goals

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,14 @@
 import * as React from 'react';
 import './App.css';
-import { database } from 'firebase';
-import { AvailableGoal } from './interfaces/db';
+import AvailableGoals from './goals/AvailableGoals';
 
-const logo = require('./logo.svg');
-
-type P = { availableGoals: AvailableGoal[] };
-
-class App extends React.Component<{}, P> {
-  constructor(props: {}) {
-    super(props);
-    this.state = { availableGoals: [] };
-    database()
-      .ref('/availableGoals')
-      .on('value', snapshot => {
-        if (!snapshot) return;
-        const availableGoals: AvailableGoal[] = snapshot.val();
-        this.setState({ availableGoals });
-      });
-  }
-
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <ul>{this.listItems()}</ul>
+        <AvailableGoals />
       </div>
     );
-  }
-
-  private listItems() {
-    return this.state.availableGoals.map(goal => <li key={goal.goal.name}>{goal.goal.name}</li>);
   }
 }
 
