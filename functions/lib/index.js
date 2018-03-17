2572073292f226d0349bd5fab4b2f04e0b9f268b
René Papesch <rene@youthtec.at>
2018-03-17 20:42:33 +0100

    implement api

---
+++ functions/lib/index.js
@@ -0,0 +1,57 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
+const figo = require("figo");
+const R = require("r-script");
+exports.dreamsPrediction = functions.https.onRequest((request, res) => {
+    // Demo client
+    var client_id = 'CaESKmC8MAhNpDe5rvmWnSkRE_7pkkVIIgMwclgzGcQY'; // Demo client ID
+    var client_secret = 'STdzfv0GXtEj_bwYn7AgCVszN1kKq5BdgEIKOM_fzybQ'; // Demo client secret
+    var connection = new figo.Connection(client_id, client_secret);
+    var access_token = '';
+    connection.credential_login('demo@figo.me', 'demo1234', null, null, null, null, function (error, token) {
+        if (error) {
+            console.error(error);
+        }
+        else {
+            get_transactions(res, token.access_token, connection);
+        }
+    });
+});
+function get_transactions(res, access_token, connection) {
+    var session = new figo.Session(access_token);
+    session.get_transactions(null, function (error, transactions) {
+        if (error) {
+            console.error(error);
+        }
+        else {
+            const transaction_csv = '';
+            const transactions_R = [{}];
+            let i = 0;
+            transactions.forEach(function (transaction) {
+                var t = [{}];
+                // Do whatever you want
+                //transaction_csv = transaction_csv + 
+                /* + ';' + transaction.name
+                 + ';' + transaction.amount
+                 + ';' + transaction.currency + "\n";*/
+                transactions_R[i] = {
+                    booking_date: transaction.booking_date,
+                    booking_text: transaction.booking_text
+                };
+                i++;
+            });
+            //res.send(transaction_csv);
+            // Write to Firebase
+            // Call R to create cashflow predictions
+            R("src/cash-prediction.R").data({ transactions: transactions_R[0],
+                nGroups: 3,
+                fxn: "mean" }).call(function (err, d) {
+                if (err)
+                    throw err;
+                res.send(d);
+            });
+        }
+    });
+}
+//# sourceMappingURL=index.js.map
