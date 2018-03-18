9c9ef804219ab9f8b57fd537680e74276383bdc3
Marion Schleifer <marion.schleifer@simplificator.com>
2018-03-18 01:33:16 +0100

    subscribed goal index

--- src/goals/SubscribedGoalItem.tsx
+++ src/goals/SubscribedGoalItem.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react';
 import * as db from '../interfaces/db';
-import { Link } from 'react-router-dom';
+import { RouteComponentProps } from 'react-router-dom';
+import { withRouter } from 'react-router';
 
 type S = {};
 type P = { goal: db.Goal };
 
-export default class SubscribedGoalItem extends React.Component<P, S> {
+class SubscribedGoalItem extends React.Component<RouteComponentProps<P> & P, S> {
   render() {
     return (
-      <div key={this.goal.id}>
+      <div style={{ cursor: 'pointer' }} onClick={() => this.props.history.push(`/goals/${this.goal.id}`)} key={this.goal.id}>
         <div style={{ textAlign: 'center' }}>
           <img style={{ width: '50%', marginTop: '10px', height: 'auto', objectFit: 'contain' }} src={this.goal.image} />
         </div>
-        <div>{this.goal.benefit}</div>
-        <div>
-          <Link to={`/goals/${this.goal.id}`}>Details</Link>
+        <div style={{ textAlign: 'center', color: '#e91e63', paddingTop: '20px', paddingBottom: '20px', fontSize: '40px' }}>
+          {this.goal.benefit}
         </div>
       </div>
     );
@@ -24,3 +24,5 @@ export default class SubscribedGoalItem extends React.Component<P, S> {
     return this.props.goal;
   }
 }
+
+export default withRouter(SubscribedGoalItem);
