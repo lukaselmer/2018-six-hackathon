82af925be66ebd6cd5b3eff95a3d4b36b78d3f10
Marion Schleifer <marion.schleifer@simplificator.com>
2018-03-18 01:01:34 +0100

    goal items

--- src/goals/SubscribedGoalItem.tsx
+++ src/goals/SubscribedGoalItem.tsx
@@ -5,11 +5,13 @@ import { Link } from 'react-router-dom';
 type S = {};
 type P = { goal: db.Goal };
 
-export default class GoalItem extends React.Component<P, S> {
+export default class SubscribedGoalItem extends React.Component<P, S> {
   render() {
     return (
-      <div>
-        <h3>{this.goal.image}</h3>
+      <div key={this.goal.id}>
+        <div style={{ textAlign: 'center' }}>
+          <img style={{ width: '50%', marginTop: '10px', height: 'auto', objectFit: 'contain' }} src={this.goal.image} />
+        </div>
         <div>{this.goal.benefit}</div>
         <div>
           <Link to={`/goals/${this.goal.id}`}>Details</Link>
