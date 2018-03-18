40c1da70779b88c227f327ea86668c0834814a95
Lukas Elmer <lukas.elmer@gmail.com>
2018-03-18 05:29:19 +0100

    Improve trophy

--- src/goals/Trophy.tsx
+++ src/goals/Trophy.tsx
@@ -12,29 +12,38 @@ export default class Trophy extends React.Component<P> {
   render() {
     return (
       <div>
-        <Card className="GoalItemCard" style={{ textAlign: 'center' }}>
+        <Card style={{ textAlign: 'center', marginTop: '30px' }}>
           <div style={{ background: '#b9b7b7', paddingTop: '20px', paddingBottom: '20px' }}>
             <div style={{ textAlign: 'center' }}>
-              <img src={trophy} style={{ width: '30%', minWidth: '150px', marginTop: '80px', marginBottom: '80px' }} />
+              <img src={trophy} style={{ width: '55%', minWidth: '150px', maxWidth: '300px' }} />
             </div>
           </div>
 
           <CardContent style={{ color: '#4d4d4e' }}>
-            <Typography variant="headline" component="h2" style={{ margin: '30px 0 0 25px', fontSize: '40px' }}>
+            <Typography variant="headline" component="h2" style={{ marginTop: '10px', fontSize: '20px' }}>
               Congratulations
             </Typography>
             <Typography
-              style={{ marginTop: '40px', fontSize: '20px', fontWeight: 300, maxWidth: '40%', marginLeft: '30%', marginRight: '30%' }}
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                marginTop: '20px',
+                fontSize: '20px',
+                fontWeight: 300
+              }}
             >
-              You reached your goal <span style={{ color: '#e91e63' }}>{this.goal.name}</span> and now you can live your dream
+              <div style={{ maxWidth: '400px' }}>
+                You reached your goal <span style={{ color: '#e91e63' }}>{this.goal.name}</span> and now you can live your dream
+              </div>
             </Typography>
           </CardContent>
 
           <div className="TrophyButtons">
-            <div style={{ margin: '15px', minWidth: '204px' }}>
+            <div style={{ margin: '5px 15px', minWidth: '204px' }}>
               <Button onClick={() => console.log('Redirect')} text="Share Your Success" />
             </div>
-            <div style={{ margin: '15px', minWidth: '204px' }}>
+            <div style={{ margin: '5px 15px 0 15px', minWidth: '204px' }}>
               <Button onClick={() => console.log('Redirect')} text="Get Your Coupon" />
             </div>
           </div>
